b05d5becfdb8b78983fcfdd6623f0252f17cfa72
erik <erikwasmosy98@gmail.com>
2024-06-20 15:49:35 -0400

    ch3 start

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+type apiConfig struct {
+	fileserverHits int
+}
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		log.Println("fileserverhits", cfg.fileserverHits)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	apiCfg := apiConfig{}
+	mux := http.NewServeMux()
+	fhandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(fhandler))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -22,4 +38,12 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	msg := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	w.Write([]byte(msg))
+}
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
 }
