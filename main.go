e8eff740adce97c9f0e7d80376fddbc030fba9b5
erik <erikwasmosy98@gmail.com>
2024-06-20 14:37:19 -0400

    11. CustomHandlers

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
-    mux.Handle("/", http.FileServer(http.Dir(".")))
-	server := http.Server{
-        Addr:    ":8080",
+	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	server := &http.Server{
+		Addr:    ":8080",
 		Handler: mux,
 	}
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
