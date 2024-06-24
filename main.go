97a29b159c0851dcb1ba576f7ba9537a94e5e9f0
erwaen <erikwasmosy98@gmail.com>
2024-06-24 13:21:08 -0400

    separete into files

--- main.go
+++ main.go
@@ -10,40 +10,31 @@ type apiConfig struct {
 	fileserverHits int
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		log.Println("fileserverhits", cfg.fileserverHits)
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+        fileserverHits: 0,
+    }
+
 	mux := http.NewServeMux()
-	fhandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(fhandler))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	fhandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/*", fhandler)
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 	log.Fatal(server.ListenAndServe())
+    log.Fatal(server.ListenAndServe())
 
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
 
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	msg := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
-	w.Write([]byte(msg))
-}
+
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits = 0
 }
