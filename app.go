929e24d05c07fb427bb83a4e0f3298e945bed82b
erwaen <erikwasmosy98@gmail.com>
2024-07-09 00:56:33 -0400

    add turso an flyio setup

--- app.go
+++ app.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/erwaen/Chirpy/database"
 	"github.com/joho/godotenv"
+      _ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type apiConfig struct {
@@ -15,10 +18,17 @@ type apiConfig struct {
 	db             *database.DB
 	jwtSecret      string
 	polkaKey       string
+	tursoDB        *sql.DB
 }
 
 func main() {
-	// Initialize the database
+	// Initialize the databasenpblock
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+
+	}
+
 	godotenv.Load()
 	jwtSecret := os.Getenv("JWT_SECRET")
 	if jwtSecret == "" {
@@ -26,7 +36,7 @@ func main() {
 	}
 	polkaKey := os.Getenv("POLKA_KEY")
 	if jwtSecret == "" {
-		log.Fatal("JWT_SECRET environment variable is not set")
+		log.Fatal("polka key environment variable is not set")
 	}
 
 	db, err := database.NewDB("./database.json")
@@ -42,11 +52,29 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+	tursoUrl := os.Getenv("TURSO_DATABASE_URL")
+	if tursoUrl == "" {
+		log.Fatal("TURSO_DATABASE_URL key environment variable is not set")
+	}
+	tursoToken := os.Getenv("TURSO_AUTH_TOKEN")
+	if tursoToken == "" {
+		log.Fatal("TURSO_AUTH_TOKEN key environment variable is not set")
+	}
+	completeUrl := tursoUrl + "?authToken=" + tursoToken
+	tursoDB, err := sql.Open("libsql", completeUrl)
+	if err != nil {
+		fmt.Println("HOLAERIK erro")
+		log.Fatal("error in connect turso db", err)
+	}
+	fmt.Println("HOLAERIK")
+	defer tursoDB.Close()
+
 	apiCfg := apiConfig{
 		fileserverHits: 0,
 		db:             db,
 		jwtSecret:      jwtSecret,
 		polkaKey:       polkaKey,
+		tursoDB:        tursoDB,
 	}
 	mux := http.NewServeMux()
 	fhandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
@@ -71,11 +99,47 @@ func main() {
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
+	mux.HandleFunc("GET /api/tursousers", apiCfg.handlerTursoUsers)
+
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 	log.Printf("Serving files from %s on port: %s\n", ".", "8080")
 	log.Fatal(server.ListenAndServe())
 
+}
+
+type TursoUser struct {
+	ID   int
+	Name string
+}
+
+func (cfg *apiConfig) handlerTursoUsers(w http.ResponseWriter, r *http.Request) {
+	rows, err := cfg.tursoDB.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	var users []TursoUser
+
+	for rows.Next() {
+		var user TursoUser
+
+		if err := rows.Scan(&user.ID, &user.Name); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return
+		}
+
+		users = append(users, user)
+		fmt.Println(user.ID, user.Name)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+
+	respondWithJson(w, 200, users)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/erwaen/Chirpy
 go 1.22.4
 
 require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240628122535-1c47b26184e8 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
 )
